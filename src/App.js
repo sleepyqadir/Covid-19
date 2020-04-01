0f4e2fefed674d5641153decc6a22cca07e742e1
AQadir64 <a.qadir67@outlook.com>
2020-04-01 16:08:37 +0500

    updates

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Layout } from "antd";
+import { Layout, Modal } from "antd";
 import { Row, Col } from "react-flexbox-grid";
 import "antd/dist/antd.css";
 import React, { Component } from "react";
@@ -8,19 +8,21 @@ import SiteStatitic from "./Components/SiteStatitic";
 import MainHeader from "./Components/MainHeader";
 import Countries from "./Components/Countries";
 import WorldMap from "./Components/WorldMap";
-
+import virus from "./imgs/virus.jpg";
+import MaintenancePage from "./Components/MaintainencePage";
 // import World from "./Components/WorldMap_1";
 
 class App extends Component {
   componentDidMount() {
     Promise.all([
-      fetch("https://node-covid19.herokuapp.com/total_ratio"),
+      fetch("https://node-covid19.herokuapp.com/tota_ratio"),
       fetch("https://node-covid19.herokuapp.com/")
     ])
       .then(([res1, res2]) => {
         return Promise.all([res1.json(), res2.json()]);
       })
       .then(([res1, res2]) => {
+        console.log(res1);
         this.setState({
           world_total_stats: res1.data,
           countries: res2.data,
@@ -32,7 +34,8 @@ class App extends Component {
     isLoading: false,
     world_total_stats: {},
     query: "",
-    Countries: []
+    Countries: [],
+    modalVisible: true
   };
   onHandleChange = e => {
     this.setState({
@@ -40,29 +43,50 @@ class App extends Component {
     });
   };
   render() {
-    const { world_total_stats, isLoading, countries } = this.state;
+    const {
+      world_total_stats,
+      isLoading,
+      countries,
+      modalVisible
+    } = this.state;
     return isLoading ? (
-      <Layout>
-        <MainHeader
-          last_update={world_total_stats.statistic_taken_at}
-          onHandleChange={this.onHandleChange}
-        />
-        <Row>
-          <Col lg={9}>
-            <Layout className="layout">
-              <SiteStatitic world_total_stats={world_total_stats} />
-              <div className="world_map">
-                <WorldMap countries={countries} />
-              </div>
-            </Layout>
-          </Col>
-          <Col lg={3}>
-            <Layout>
-              <Countries query={this.state.query} countries={countries} />
-            </Layout>
-          </Col>
-        </Row>
-      </Layout>
+      world_total_stats.statistic_taken_at ? (
+        <Layout>
+          <Modal
+            visible={modalVisible}
+            onCancel={() => {
+              this.setState({
+                modalVisible: false
+              });
+            }}
+            footer={[]}
+            width={670}
+          >
+            <img src={virus} alt="precaution" style={{ marginTop: "20px" }} />
+          </Modal>
+          <MainHeader
+            last_update={world_total_stats.statistic_taken_at}
+            onHandleChange={this.onHandleChange}
+          />
+          <Row>
+            <Col lg={9}>
+              <Layout className="layout">
+                <SiteStatitic world_total_stats={world_total_stats} />
+                <div className="world_map">
+                  <WorldMap countries={countries} />
+                </div>
+              </Layout>
+            </Col>
+            <Col lg={3}>
+              <Layout>
+                <Countries query={this.state.query} countries={countries} />
+              </Layout>
+            </Col>
+          </Row>
+        </Layout>
+      ) : (
+        <MaintenancePage />
+      )
     ) : (
       <div className="loader">
         <img src={Logo} className="main_logo" alt="Covid-19" />
